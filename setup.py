ce9975be7927cc45f9ea0403adee97ca7fc51793
Jens Vagelpohl <jens@plyp.com>
2025-12-03 17:29:41 +0100

    - Move package metadata from setup.py to pyproject.toml (#36)

--- setup.py
+++ setup.py
@@ -15,53 +15,5 @@
 from setuptools import setup
 
 
-def read(name):
-    with open(name) as f:
-        return f.read()
-
-
-setup(
-    name='Products.SiteErrorLog',
-    version='7.1.dev0',
-    url='https://github.com/zopefoundation/Products.SiteErrorLog',
-    project_urls={
-        'Issue Tracker': ('https://github.com/zopefoundation/'
-                          'Products.SiteErrorLog/issues'),
-        'Sources': 'https://github.com/zopefoundation/Products.SiteErrorLog',
-    },
-    license='ZPL-2.1',
-    description='Error log for Zope.',
-    author='Zope Foundation and Contributors',
-    author_email='zope-dev@zope.dev',
-    long_description='\n'.join([read('README.rst'),
-                                read('CHANGES.rst')]),
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Zope Public License',
-        'Framework :: Zope',
-        'Framework :: Zope :: 5',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Programming Language :: Python :: 3.13',
-        'Programming Language :: Python :: 3.14',
-        'Programming Language :: Python :: Implementation',
-        'Programming Language :: Python :: Implementation :: CPython',
-    ],
-    python_requires='>=3.10',
-    install_requires=[
-        'AccessControl',
-        'Acquisition',
-        'transaction',
-        'zExceptions',
-        'Zope >= 4.0b9.dev0',
-        'zope.component',
-        'zope.interface',
-        'zope.event',
-    ],
-    include_package_data=True,
-)
+# See pyproject.toml for package metadata
+setup()
